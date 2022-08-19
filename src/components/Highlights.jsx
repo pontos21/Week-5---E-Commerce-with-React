d18b7c1c53f3c16e4e34c00aba7a75ac45b5345c
paul serafim <paul.serafim.3021@gmail.com>
2022-08-19 14:36:53 +1000

    code

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Highlight from './ui/Highlight';
 
 const Highlights = () => {
     return (
@@ -10,38 +11,22 @@ const Highlights = () => {
                         Why choose <span className="purple">Library ?</span>
                     </h2>
                     <div className="highlight__wrapper">
-                        <Highlight icon={<FontAwesomeIcon icon="bolt" />}/>
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                
-                            </div>
-                            <h3 className="highlight__subtitle">Easy and Quick</h3>
-                            <p className="highlight__para">
-                                Get access to the book you purchased online instantly.
-                            </p>
-                        </div>
+                        <Highlight icon={<FontAwesomeIcon icon="bolt" />} 
+                        title="Easy and Quick"
+                        para="Get access to the book you purchased online instantly."
+                        />
                     </div>
                     <div className="highlight__wrapper">
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="book-open" />
-                            </div>
-                            <h3 className="highlight__subtitle">10,000+ Books</h3>
-                            <p className="highlight__para">
-                                Library has books in all your favourite categories.
-                            </p>
-                        </div>
+                        <Highlight icon={<FontAwesomeIcon icon="bolt" />} 
+                        title="Easy and Quick"
+                        para="Get access to the book you purchased online instantly."
+                        />
                     </div>
                     <div className="highlight__wrapper">
-                        <div className="highlight">
-                            <div className="highlight__img">
-                                <FontAwesomeIcon icon="tags" />
-                            </div>
-                            <h3 className="highlight__subtitle">Affordable</h3>
-                            <p className="highlight__para">
-                                Get your hands on popular books for as little as $10
-                            </p>
-                        </div>
+                        <Highlight icon={<FontAwesomeIcon icon="book-open" />} 
+                        title="10,000+ Books"
+                        para="Library has books in all your favourite categories."
+                        />
                     </div>
                 </div>
             </div>
