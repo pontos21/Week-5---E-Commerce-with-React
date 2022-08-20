b9d3ff57abe9944ad8778c1fa3511f858ed04436
paul serafim <paul.serafim.3021@gmail.com>
2022-08-20 16:48:09 +1000

    code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 
 import Nav from './components/Nav'
-import Landing from './components/Landing'
-import Highlights from './components/Highlights'
-import Featured from './components/Featured'
-import Discounted from './components/Discounted'
-import Explore from './components/Explore'
 import Footer from './components/Footer'
+import Home from './pages/Home'
+import Books from './pages/Books'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { books } from "./data"
+import  BookInfo  from "./pages/BookInfo"
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />}/>
+      <Route path="./books/1" render={() => <BookInfo books={books}/>}/>
       <Footer />
     </div>
+    </Router>
+    
   );
 }
 
