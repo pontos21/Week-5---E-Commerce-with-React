8688d9f009ee0d91d034cbad8e669dccec18b62a
paul serafim <paul.serafim.3021@gmail.com>
2022-08-20 17:56:12 +1000

    more code

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const BookInfo = ({ books }) => {
     return (
@@ -7,7 +9,9 @@ const BookInfo = ({ books }) => {
                 <div className="books__container">
                     <div className="row">
                         <div className="book__selected--top">
-                            
+                            <Link to="./books" className="book__link">
+                              <FontAwesomeIcon icon="arrow-left" />  
+                            </Link>
                         </div>
                     </div>
                 </div>
