5e4faacfd92d901abc67587a59736cd3c94b78f1
paul serafim <paul.serafim.3021@gmail.com>
2022-08-25 15:51:21 +1000

    updated code again

--- src/App.js
+++ src/App.js
@@ -43,7 +43,6 @@ function App() {
   }
 
   useEffect(() => {
-    console.log(cart)
   }, [cart])
 
   return (
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -11,27 +11,26 @@ const Highlights = () => {
                         Why choose <span className="purple">Library ?</span>
                     </h2>
                     <div className="highlight__wrapper">
-                        <Highlight icon={<FontAwesomeIcon icon="bolt" />} 
-                        title="Easy and Quick"
-                        para="Get access to the book you purchased online instantly."
-                        />
-                    </div>
-                    <div className="highlight__wrapper">
-                        <Highlight icon={<FontAwesomeIcon icon="book-open" />} 
-                        title="Easy and Quick"
-                        para="Get access to the book you purchased online instantly."
-                        />
-                    </div>
-                    <div className="highlight__wrapper">
-                        <Highlight icon={<FontAwesomeIcon icon="tags" />} 
-                        title="10,000+ Books"
-                        para="Library has books in all your favourite categories."
-                        />
+                        <Highlight 
+                            icon={<FontAwesomeIcon icon="bolt" />} 
+                            title="Easy and Quick"
+                            para="Get access to the book you purchased online instantly." 
+                            />
+                            <Highlight 
+                            icon={<FontAwesomeIcon icon="book-open" />} 
+                            title="10,000+ Books"
+                            para="Library has books in all your favourite categories." 
+                            />
+                            <Highlight 
+                            icon={<FontAwesomeIcon icon="tags" />} 
+                            title="Afforable"
+                            para="Get your hands on popular books for as little as $10" 
+                            />
                     </div>
                 </div>
             </div>
         </section>
-    );
+    )
 }
 
 export default Highlights;
